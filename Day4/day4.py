f36925df2fe18092f754c11e1f8a1cdbe978b2ae
Daniel Sauvé <djsauve@rogers.com>
2018-12-04 19:23:48 -0500

    Finish Day 4 Part 1

---
+++ Day4/day4.py
@@ -0,0 +1,44 @@
+import re
+
+
+def longest_sleeper(notes):
+    notes.sort()
+    total_sleep = {}
+    minute_sleep = {}
+    time_regex = re.compile("(\d\d):(\d\d)")
+    guard, start = -1, -1
+    for note in notes:
+        if "begins shift" in note:
+            guard = int(note.split()[3].replace("#", ""))
+            start = -1
+        elif "falls asleep" in note:
+            start = int(time_regex.search(note).group(2))
+        elif "wakes up" in note:
+            end = int(time_regex.search(note).group(2))
+            if guard not in total_sleep.keys():
+                total_sleep[guard] = end - start
+            else:
+                total_sleep[guard] += end - start
+            for minute in range(start, end):
+                if (guard, minute) not in minute_sleep.keys():
+                    minute_sleep[(guard, minute)] = 1
+                else:
+                    minute_sleep[(guard, minute)] += 1
+            start = -1
+    max_sleep = -1
+    for key in total_sleep.keys():
+        if total_sleep[key] > max_sleep:
+            max_sleep = total_sleep[key]
+            guard = key
+    max_sleep = -1
+    minute = -1
+    for key in minute_sleep.keys():
+        if key[0] == guard and minute_sleep[key] > max_sleep:
+            max_sleep = minute_sleep[key]
+            minute = key[1]
+    return guard * minute
+
+
+if __name__ == "__main__":
+    problemInput = open("./day4.txt").read()
+    print(longest_sleeper(problemInput.split("\n")))
