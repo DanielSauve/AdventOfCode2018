c22887ef43924e089cdd7c685a8cdd50df8832fb
Daniel Sauvé <djsauve@rogers.com>
2018-12-04 19:38:02 -0500

    Finish Day 4 Part 2
    This was easier since I did a lot of optimization for part 1 that made part 2 very simple

--- Day4/day4.py
+++ Day4/day4.py
@@ -39,6 +39,36 @@ def longest_sleeper(notes):
     return guard * minute
 
 
+def most_consistent(notes):
+    notes.sort()
+    minute_sleep = {}
+    time_regex = re.compile("(\d\d):(\d\d)")
+    guard, start = -1, -1
+    for note in notes:
+        if "begins shift" in note:
+            guard = int(note.split()[3].replace("#", ""))
+            start = -1
+        elif "falls asleep" in note:
+            start = int(time_regex.search(note).group(2))
+        elif "wakes up" in note:
+            end = int(time_regex.search(note).group(2))
+            for minute in range(start, end):
+                if (guard, minute) not in minute_sleep.keys():
+                    minute_sleep[(guard, minute)] = 1
+                else:
+                    minute_sleep[(guard, minute)] += 1
+            start = -1
+    max_sleep = -1
+    minute = -1
+    for key in minute_sleep.keys():
+        if minute_sleep[key] > max_sleep:
+            max_sleep = minute_sleep[key]
+            guard = key[0]
+            minute = key[1]
+    return guard * minute
+
+
 if __name__ == "__main__":
     problemInput = open("./day4.txt").read()
     print(longest_sleeper(problemInput.split("\n")))
+    print(most_consistent(problemInput.split("\n")))
