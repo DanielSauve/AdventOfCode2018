57fceb6015df8035ba80498116520c4b1d8b2fa7
Daniel Sauve <djsauve@rogers.com>
2018-12-08 18:03:01 -0500

    Day 8 Part 2
    There is a lot of duplicate code now, this can be easily fixed

--- Day8/day8.py
+++ Day8/day8.py
@@ -34,7 +34,47 @@ def child_metadata(tree):
     return metadata_sum, next_child + metadata_count
 
 
+def root_value(tree):
+    tree = list(map(lambda x: int(x), tree))
+    val = 0
+    children_count = tree[0]
+    metadata_count = tree[1]
+    next_child = 2
+    child_values = []
+    for i in range(children_count):
+        child_val, next_count = node_value(tree[next_child:])
+        next_child += next_count
+        child_values.append(child_val)
+    for i in range(metadata_count):
+        child = tree[next_child + i]
+        if child - 1 < len(child_values):
+            val += child_values[child - 1]
+    return val
+
+
+def node_value(tree):
+    val = 0
+    children_count = tree[0]
+    metadata_count = tree[1]
+    next_child = 2
+    if children_count == 0:
+        for i in range(metadata_count):
+            val += tree[2 + i]
+        return val, 2 + metadata_count
+    child_values = []
+    for i in range(children_count):
+        child_val, next_count = node_value(tree[next_child:])
+        next_child += next_count
+        child_values.append(child_val)
+    for i in range(metadata_count):
+        child = tree[next_child + i]
+        if child - 1 < len(child_values):
+            val += child_values[child - 1]
+    return val, next_child + metadata_count
+
+
 if __name__ == "__main__":
     problemInput = open("./day8.txt").read()
     sys.setrecursionlimit(10000)
     print(sum_metadata(problemInput.split()))
+    print(root_value(problemInput.split()))
